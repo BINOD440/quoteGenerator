9336260187b4b6b144d5436a8de378c705f7aa4a
BINOD440 <69833029+BINOD440@users.noreply.github.com>
2023-01-25 21:30:16 +0530

    Quote Generator picks motivation quote to motivate you
    Help you to get motivated with the liitle of html , css , js and api.

---
+++ script.js
@@ -0,0 +1,64 @@
+function startTime() {
+    var today=new Date();
+    var h=today.getHours();
+    var m=today.getMinutes();
+    var s=today.getSeconds();
+    var ampm = "";
+    m = checkTime(m);
+
+    if (h > 12) {
+      h = h - 12;
+      ampm = " PM";
+    } else if (h == 12){
+        h = 12;
+      ampm = " AM";
+    } else if (h < 12){
+        ampm = " AM";
+    } else {
+        ampm = "PM";
+    };
+  
+  if(h==0) {
+    h=12;
+  }
+    
+    document.getElementById('display').innerHTML = h+":"+m+ampm;
+    var t = setTimeout(function(){startTime()},500);
+}
+
+function checkTime(i) {
+    if (i<10) {i = "0" + i};  // add zero in front of numbers < 10
+    return i;
+}
+
+function genQuote() {
+  var randNum = Math.floor(Math.random() * 8) + 1;
+  document.getElementById('quote').innerHTML = quotes[randNum];
+  var tweetQuote = quotes[randNum].split(' ').join('%20');
+  tweetQuote = tweetQuote.split('<br>').join('');
+  tweetQuote = "https://twitter.com/intent/tweet?text=" + tweetQuote.split('"').join('')
+  $('.twitter-share-button').attr('href', tweetQuote);
+}
+function startDate() {
+  var d = new Date();
+  var days = ["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"];
+  document.getElementById("date").innerHTML = days[d.getDay()]+" | "+[d.getMonth()+1]+"/"+d.getDate()+"/"+d.getFullYear();
+}
+
+let quote = document.getElementById("quote");
+let author = document.getElementById("author");
+let btn = document.getElementById("btn");
+
+const url = "https://api.quotable.io/random";
+
+let getQuote = () => {
+  fetch(url)
+    .then((data) => data.json())
+    .then((item) => {
+      quote.innerText = item.content;
+      author.innerText = item.author;
+    });
+};
+
+window.addEventListener("load", getQuote);
+btn.addEventListener("click", getQuote);
